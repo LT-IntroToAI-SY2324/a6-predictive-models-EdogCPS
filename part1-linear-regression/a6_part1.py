8ec9af0f2106a8f3f256548357fc2c876beaf261
Ewan <ejmcphilliam@cps.edu>
2023-11-30 10:47:51 -0600

    modified:   part1-linear-regression/a6_part1.py 	modified:   part1-linear-regression/part1_writeup.md 	new file:   part2-training-testing-data/blood_pressure_data.csv

--- part1-linear-regression/a6_part1.py
+++ part1-linear-regression/a6_part1.py
@@ -11,14 +11,34 @@ y = data["Blood Pressure"].values
 x = x.reshape(-1,1)
 
 # Create the model
+model = LinearRegression().fit(x, y)
 
 # Find the coefficient, bias, and r squared values. 
 # Each should be a float and rounded to two decimal places. 
-
+coef = round(float(model.coef_), 2)
+intercept = round(float(model.intercept_), 2)
+r_squared = model.score(x, y)
 
 # Print out the linear equation and r squared value
+print(f"Model's Linear Equation: y = {coef}x + {intercept}")
+print(f"R Squared value: {r_squared}")
 
 # Predict the the blood pressure of someone who is 43 years old.
 # Print out the prediction
+x_predict = 43
+prediction = model.predict([[x_predict]])
+print(f"The predicted blood presure of somebody {x_predict} years old is {prediction} mmHg.")
 
 # Create the model in matplotlib and include the line of best fit
+plt.figure(figsize=(6,4))
+plt.scatter(x,y, c="purple")
+plt.scatter(x_predict, prediction, c="blue")
+
+plt.xlabel("Age in years")
+plt.ylabel("Blood Presure in mmHg")
+plt.title("Blood Presuue in mmHg by Age in years")
+
+plt.plot(x, coef*x + intercept, c="r", label="Line of Best Fit")
+
+plt.legend()
+plt.show()
--- part1-linear-regression/part1_writeup.md
+++ part1-linear-regression/part1_writeup.md
@@ -5,7 +5,11 @@ After completing `a6_part1.py` answer the following questions
 ## Questions to answer
 
 1. What is the r squared value?  What does this say about this lenear regression model?
+The r sqaured value is the correlation coefiient. it tells us how correletated the data is. 1 or -1 is extremly correcleted and 0 is no correlation. + means positive slope and - means negative slope. 
 
 2. According to your model, what is the predicted systolic blood pressure for a person iwho is 43 years old?
+137.46 mmHg. 
 
 3. How accurate do you think this model's predictions are?  Do you think this model is accurate enough to reliably predict someone's blood pressure based on their age?  Why or why not?  And if not, what could improve the model?
+
+I think the model is acurate enough to predict within the provided data range. Outside of the selected data reange would be extrapolation. More data could alwasy improve the model.
